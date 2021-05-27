3431ba2c2850b9d871f248c112438222f1cae59b
s-m-kashani98 <s.m.kashani98@gmail.com>
2021-05-26 23:30:54 -0500

    Create Finder.py

---
+++ Finder.py
@@ -0,0 +1,39 @@
+
+loop= []
+def addToLoop(x):
+    for i in loop:
+        if i == x:
+            return True    
+    loop.append(x)
+    return False
+def makeStep(a,pos):
+    temp = []
+    for i in a:
+        if pos[1] == i[0]:
+            temp.append(i)
+    return temp
+
+def loopFinder(a,pos):
+    step = makeStep(a,pos)
+    for k in step:
+        x = addToLoop(k)
+        if(x):
+            return True
+        else:
+            if loopFinder(a,k):
+                return True
+            else:
+                loop.pop()
+    return False
+
+def thereIsLoop(a):
+    flag = False
+    for i in a:
+        loop.append(i)
+        if loopFinder(a,i):
+            flag = True
+            break    
+        loop.pop()
+    return flag
+
+
